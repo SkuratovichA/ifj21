254366a7b63f3fdd4c903be8ca827bedc34397f7
SkuratovichA <skuratovich.aliaksandr@gmail.com>
2021-10-11 07:52:12 +0200

    [ifj21] add tests.

--- ifj21.c
+++ ifj21.c
@@ -4,13 +4,20 @@
 #include "progfile.h"
 
 int main() {
-    printf("Hello!\n");
-
     pfile_t *pfile;
+
+    if (DEBUG) {
+        // tests
+        pfile_t *pf1 = Pfile.ctor("require \"ifj21\"\n");
+        Parser.analyse(pf1);
+        debug_msg_s("Tests are working!\n");
+        Pfile.free(pf1);
+    }
 
     if (!(pfile = Pfile.getfile_stdin())) {
         return Errors.get_error();
     }
+
 
     if (Parser.analyse(pfile)) {
         return Errors.get_error();
