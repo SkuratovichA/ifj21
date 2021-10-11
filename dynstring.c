c897e0674d19326a8fef736625fcf381fc325513
SkuratovichA <skuratovich.aliaksandr@gmail.com>
2021-10-11 09:42:38 +0200

    [dynstring] https://github.com/SkuratovichA/ifj21/pull/5/files/893d202f473552ea9ca439aace6e531370004c75#r725838449

--- dynstring.c
+++ dynstring.c
@@ -34,7 +34,7 @@ static dynstring_t Str_create(const char *s) {
 /**
  * @brief Get char * (c dynstring_t ending with '\0') from dynstring_t.
  *
- * @param str
+ * @param str dynstring_t object.
  * @return c dynstring_t representation.
  */
 static char *Str_c_str(dynstring_t str) {
@@ -45,7 +45,7 @@ static char *Str_c_str(dynstring_t str) {
 /**
  * @brief Return length of given dynstring_t.
  *
- * @param str
+ * @param str dynstring_t object.
  * @return len of dynstring_t
  */
 static size_t Str_length(dynstring_t *str) {
@@ -98,8 +98,8 @@ static void Str_append_char(dynstring_t *str, char ch) {
 /**
  * @brief Compare dynstring_t and char* using strcmp.
  *
- * @param s
- * @param s1
+ * @param s1 dynstring_t object.
+ * @param s2 dynstring_t object.
  * @returns -1, 0, 1 depends on lexicographical ordering of two strings.
  */
 static int Str_cmp(dynstring_t s1, dynstring_t s2) {
@@ -113,8 +113,8 @@ static int Str_cmp(dynstring_t s1, dynstring_t s2) {
 /**
  * @brief Concatenate two dynstrings in the not very efficient way.
  *
- * @param s1
- * @param s2
+ * @param s1 dynstring_t object.
+ * @param s2 dynstring_t object.
  * @returns new dysntring, which is product of s1 and s2.
  */
 static dynstring_t Str_cat(dynstring_t *s1, dynstring_t *s2) {
