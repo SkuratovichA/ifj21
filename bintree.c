b86fb1d7594ea8a0c54a03eb7bfd84f7a4184035
jakubKuznik <kuznja16@gmail.com>
2021-10-17 15:33:03 +0200

    Update bintree.c

--- bintree.c
+++ bintree.c
@@ -4,7 +4,7 @@
 /**
  * @brief Finds a node in a tree. The function uses a string to find the node.
  *
- * @param tree Pointer to first node of tree.
+ * @param tree Pointer to first node of the tree.
  * @param token string name that we are looking for.
  *
  * @return Pointer to node or NULL if can not find.
