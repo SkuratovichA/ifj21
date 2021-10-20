e7b08c7d04010f2e40db9d68b028cf93c1483348
achxkloel <evgeny.torbin49@gmail.com>
2021-10-20 22:18:01 +0200

    [expressions] update old changes (this is working now)

--- expressions.h
+++ expressions.h
@@ -4,6 +4,27 @@
 #include "macros.h"
 #include "list.h"
 
+/**
+ * Add prefix before operator name.
+ */
+#define OP(name) _cat_2_(OP, name)
+
+/**
+ * List of operators from the precedence table.
+ */
+typedef enum op_list {
+    OP(ID),
+    OP(LPAREN),
+    OP(RPAREN),
+    OP(HASH),
+    OP(MUL_GROUP),
+    OP(ADD_GROUP),
+    OP(STRCAT),
+    OP(FUNC),
+    OP(COMMA),
+    OP(DOLLAR)
+} op_list_t;
+
 struct expr_interface_t {
     bool (*parse)(pfile_t *);
 };
