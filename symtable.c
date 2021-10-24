58d85894a5dcf69cda0fdb4bd9114246a3629f0b
SkuratovichA <skuratovich.aliaksandr@gmail.com>
2021-10-24 22:32:37 +0200

    [symtable] fix, add tests

--- symtable.c
+++ symtable.c
@@ -1,4 +1,14 @@
 #include "symtable.h"
+#include "errors.h"
+#include "scanner.h"
+#include "tests/tests.h"
+
+struct symtable {
+    symtable_t *parent;    /**< */
+    symtable_t *child;     /**< */
+    struct node_t *tree;   /**< */
+    int scope_index;       /**< */
+};
 
 /**
  * @brief
@@ -6,7 +16,12 @@
  * @param
  * @return
  */
-static sym_t *Ctor(sym_table *table, token_t *token) {
+static symtable_t *Ctor(symtable_t *table, token_t token) {
+    debug_msg("implement me.\n");
+    debug_msg("Got token: %s '%s'\n", Scanner.to_string(token.type),
+              (token.type == TOKEN_STR || token.type == TOKEN_ID) ? Dynstring.c_str(token.attribute.id) : ""
+    );
+
     /*
     node_t *node = calloc(1, sizeof(node_t));
     if (node == NULL) {
@@ -28,13 +43,21 @@ static sym_t *Ctor(sym_table *table, token_t *token) {
     return NULL;
 }
 
-
-
-
 /**
  * Symtable interface.
  */
-
-const struct sym_table_op_struct Symtable = {
+const struct symtable_interface_t Symtable = {
         .ctor = Ctor,
 };
+
+#ifdef SELFTEST_symtable
+
+int main() {
+    debug_msg("symtable selfdebug\n");
+    token_t tok = { .type = TOKEN_STR, .attribute.id = Dynstring.ctor("\"Test name\"") };
+    Symtable.ctor(NULL, tok);
+
+    return 0;
+}
+
+#endif
--- symtable.h
+++ symtable.h
@@ -2,36 +2,15 @@
 
 // Includes
 #include "scanner.h"
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdarg.h>
-#include <assert.h>
-#include <ctype.h>
-#include <math.h>
-#include <stdint.h>
 #include "debug.h"
 #include "errors.h"
 
 
-extern struct token_t;
+typedef struct symtable symtable_t;
 
-typedef struct sym_table {
-    struct sym_table *parent;        /**< */
-    struct sym_table *child;         /**< */
-    struct node_t *tree;             /**< */
-    int scope_index;                 /**< */
-} sym_table;
-
-typedef struct sym_table sym_t;
-
-
-extern const struct sym_table_op_struct Symtable;
-
-struct sym_table_op_struct {
-
-    sym_t *(*ctor)(sym_t *, token_t *);
-
-
+struct symtable_interface_t {
+    symtable_t *(*ctor)(symtable_t *, token_t);
 };
+
+extern const struct symtable_interface_t Symtable;
+
