ba99999fce06d244c5b849b9f9aad8f44200927a
svobodovaLucie <xsvobo1x@stud.fit.vutbr.cz>
2021-11-11 16:44:52 +0100

    [generator] add the list of instructions

--- ifj21.c
+++ ifj21.c
@@ -3,6 +3,10 @@
 #include "parser.h"
 #include "progfile.h"
 #include "symtable.h"
+// moved definition of list_item_t to list.h
+#include "list.h"
+#include "code_generator.h"
+
 
 int main() {
     pfile_t *pfile;
@@ -12,10 +16,22 @@ int main() {
         return Errors.get_error();
     }
 
+    instrList = List.ctor();            // creating global list of instructions
+    tmp_instr = Dynstring.ctor("");     // variable for currently generated instruction
+
     if (Parser.analyse(pfile)) {
         ret = Errors.get_error();
     }
 
+    Generator.prog_start();             // test - remove later
+    // Prints the list of instructions to stdout
+    printf("# ---------- Instructions List ----------\n");
+    for (list_item_t *tmp = instrList->head; tmp != NULL; tmp = tmp->next ) {
+        printf("%s\n", Dynstring.c_str(tmp->data));
+    }
+
+    List.dtor(instrList, Dynstring.dtor); // use Dynstring.dtor or free?
+    Dynstring.dtor(tmp_instr);
 
     Pfile.dtor(pfile);
     return ret;
