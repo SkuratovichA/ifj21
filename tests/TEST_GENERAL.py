45a4a731027dd1ddfcd904c89538b2a939da9879
jakubKuznik <kuznja16@gmail.com>
2021-11-14 15:06:48 +0100

    [test] PYTHON TEST SCRIPT

--- tests/TEST_GENERAL.py
+++ tests/TEST_GENERAL.py
@@ -20,30 +20,64 @@ from colorama import Fore, Style
 test_num = 0
 
 
-def test(input_file, expected_error_code):
+#return first two char from string 
+def firstTwo(string):
+    return string[:2]
 
-    with open(input_file, 'r') as file:
-        data = file.read().rstrip()
-
-
-    result = subprocess.run(
-        ["../cmake-build-debug/ifj21"], text=True, input=data
-    ).returncode
-
-    if result != expected_error_code:
-        print(Fore.RED + "[" , test_num , "]" + "   Failed    [test:]" + input_file)
-    else:
-        print(Fore.GREEN + "[" , test_num , "]" + "   Passed    [test:]" + input_file)
-    test = test_num+1
+# run all test from input folder 
+# Expected error code should be same as input 
+def test_error_codes(expected_error_code, input_folder, test_set):
     
+    global test_num
+    
+    #open every file in folder 
+    for filename in os.listdir(input_folder):
+        with open(os.path.join(input_folder, filename), 'r') as f:
+            #set file contain as input
+            file_num = firstTwo(filename)
+            data = f.read()
+            
+            test_num += 1
+            
+            #run ifj21 with data on stdin 
+            result = subprocess.run(
+                ["../cmake-build-debug/ifj21"], text=True, input=data
+            ).returncode
 
+            if result != expected_error_code:
+                #[global num]   FAILED || PASED     EXPECTED  X   ACTUALL  X   TEST_SET   FILE_NUM
+                print(Fore.RED + "[" ,test_num, "]" + "  Failed   " + " exp ret code: ",  expected_error_code , "Actuall:", result, test_set, "file_num",file_num)
+            else:
+                print(Fore.GREEN + "[" , test_num , "]" + "   Passed  "  + test_set)
 
 
 def main():
     #subprocess.run(args, *, stdin=None, input=None, stdout=None, stderr=None, capture_output=False, shell=False, cwd=None, timeout=None, check=False, encoding=None, errors=None, text=None, env=None, universal_newlines=None, **other_popen_kwargs)
     print(Fore.RESET)
-    test('test_inputs_valid/0_vypocet_faktorialu_iterativne.tl', 1)
-    test('test_inputs_invalid/1_lexical/1_tokens_invalid.tl', 0)
+
+    #scanner test 
+    test_error_codes(1, "test_inputs_invalid/1_lexical", "1_LEXICAL")
+    #syntax test 
+    test_error_codes(2, "test_inputs_invalid/2_syntax", "2_SYNTAX" )
+    #definiton
+    test_error_codes(3, "test_inputs_invalid/3_definition", "3_DEFINITION" )
+    #type_missmatch
+    test_error_codes(4, "test_inputs_invalid/4_type_missmatch", "4_TYPE_MISSMATCH" )
+    #function_sematics
+    test_error_codes(5, "test_inputs_invalid/5_function_sematics", "5_FUNCTION_SEMATICS" )
+    #incompatable_sematics
+    test_error_codes(6, "test_inputs_invalid/6_type_incompatable_sematic", "6_INCOMPATABLE_SEMATICS" )
+    #sematic_other
+    test_error_codes(7, "test_inputs_invalid/7_sematic_other", "7_SEMATIC_OTHER" )
+    #runtime_nil
+    test_error_codes(8, "test_inputs_invalid/8_runtime_nil", "8_RUNTIME_NIL" )
+    #zero_divide
+    test_error_codes(9, "test_inputs_invalid/9_zero_divide", "9_ZERO_DIVIDE" )
+    
+    #VALID PROGRAMS 
+    test_error_codes(0, "test_inputs_valid", "0_VALID" )
+    
+    
     print(Fore.RESET)
 
     return 0
