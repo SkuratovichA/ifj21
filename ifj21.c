dc5a33d7a0d58a55fc922375ef8afe0aba025e42
SkuratovichA <skuratovich.aliaksandr@gmail.com>
2021-11-17 01:17:28 +0100

    [ifj21] delete unnecessary if statement

--- ifj21.c
+++ ifj21.c
@@ -28,19 +28,20 @@ int main() {
     debug_msg("initializing generator(fun on address %p)", (void *) Generator.initialise);
     Generator.initialise();
 
-    if (Parser.analyse(pfile)) {
-        ret = Errors.get_error();
-    }
+    Parser.analyse(pfile);
+
+    // ERROR_NOERROR has value 0
+    ret = Errors.get_error();
 
     // Print instructions only when everything was ok
     printf("\n\n# <<<<<<<<<< Return code: %d >>>>>>>>>>\n\n\n", ret);
     //if (ret == 0) {
     // Prints the list of instructions to stdout
-        printf("# ---------- Instructions List ----------\n");
-        INSTR_CHANGE_ACTIVE_LIST(instructions.startList);
-        printf("# ---------- startList ----------\n");
-        for (list_item_t *tmp = instrList->head; tmp != NULL; tmp = tmp->next) {
-            printf("%s\n", Dynstring.c_str(tmp->data));
+    printf("# ---------- Instructions List ----------\n");
+    INSTR_CHANGE_ACTIVE_LIST(instructions.startList);
+    printf("# ---------- startList ----------\n");
+    for (list_item_t *tmp = instrList->head; tmp != NULL; tmp = tmp->next) {
+        printf("%s\n", Dynstring.c_str(tmp->data));
         }
         INSTR_CHANGE_ACTIVE_LIST(instructions.instrListFunctions);
         printf("# ---------- listFunctions ----------\n");
